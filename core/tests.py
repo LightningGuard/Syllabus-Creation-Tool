a9c07b814fa010fa058192db37fa604dac48ad4e
Benjamin Wright <egonor@outlook.com>
2021-10-19 16:42:09 -0400

    Unit Tests for Urls and Templates loaded

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+client = Client()
+
+
+class TestIndexPage(TestCase):
+    def test_IndexUrl(self):
+        response = client.get(reverse('index'))
+        assert (response.status_code == 200)
+
+    def test_IndexTemplates(self):
+        response = client.get(reverse('index'))
+        assert (response.templates[0].name == 'index.html')
+        assert (response.templates[1].name == 'base_generic.html')
+
+
+class TestStudentPage(TestCase):
+    def test_StudentUrl(self):
+        response = client.get(reverse('student'))
+        assert (response.status_code == 200)
+
+    def test_StudentTemplates(self):
+        response = client.get(reverse('student'))
+        assert (response.templates[0].name == 'student.html')
+        assert (response.templates[1].name == 'base_generic.html')
+
+
+class TestInstructorPage(TestCase):
+    def test_InstructorUrl(self):
+        response = client.get(reverse('instructor'))
+        assert (response.status_code == 200)
+
+    def test_InstructorTemplates(self):
+        response = client.get(reverse('instructor'))
+        assert (response.templates[0].name == 'instructor.html')
+        assert (response.templates[1].name == 'base_generic.html')
