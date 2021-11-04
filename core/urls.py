7c5432a6399ec681ff77c89f950d8111d4fcbbd6
Jamesf15182 <60937926+Jamesf15182@users.noreply.github.com>
2021-11-04 15:56:47 -0400

    Created a way that faculty members can create a syllabus template. This is done by the instructor entering their course related info and hitting submit to see a preview of what it would look like.

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from core import views
 
 urlpatterns = [
+
     path('', views.index, name='index'),
     path('student', views.student, name='student'),
     path('instructor', views.instructor, name='instructor'),
+    path('createSyllabus', views.createSyllabus, name='createSyllabus'),
+    path('syllabusViewer', views.syllabusViewer, name='syllabusViewer'),
+    path('addSyllabus', views.addSyllabus, name='addSyllabus'),
+
 ]
