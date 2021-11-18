be442b0d681a4b0237ac19705d0a57207316ca1e
Destin22 <dkrepps1@umbc.edu>
2021-11-18 12:31:18 -0500

    Redesigned website for better visuals and navigation QoL, moved email password to a secure location rather than out in the open, and collaborated with james to implement email system in his previously completed web app.

--- syllabustool/settings.py
+++ syllabustool/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_PASSWORD = 'Znqbdyq8'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_USER = 'SyllabusToolAcc@gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
