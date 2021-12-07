58a7d6b67eebba6e69b04fc721ec17956bc50810
Jamesf15182 <60937926+Jamesf15182@users.noreply.github.com>
2021-12-07 15:59:07 -0500

    Update tests.py

--- core/tests.py
+++ core/tests.py
@@ -341,7 +341,7 @@ class TestCalendarFunctionality(StaticLiveServerTestCase):
 
         self.browser.find_element_by_xpath('/html/body/div[2]/div[1]/div[3]/div[15]').click()
         time.sleep(3)
-        self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/input').send_keys(' & CMSC 447 Exam')
+        self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/input').send_keys('CMSC 447 Exam')
         time.sleep(3)
         self.browser.find_element_by_xpath('/html/body/div[2]/div[2]/button[1]').click()
         time.sleep(3)
